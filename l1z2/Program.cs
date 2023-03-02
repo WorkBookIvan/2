e77cb42388bffa203b53e99c645f5e9c41e79c2d
Ivan Baybikov <WorkBookivan@gmail.com>
2023-03-02 14:28:57 +0300

    zad 4

---
+++ l1z2/Program.cs
@@ -0,0 +1,11 @@
+﻿// На ввод принимает число и выводит его квадрат .
+// 4; -3; -7;
+// result=x*x
+// 
+
+double a;
+a = -7;
+double result ;
+ result = a * a ;
+System.Console.WriteLine(result);
+
